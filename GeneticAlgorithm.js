b1f08b0d0197cbc6ccedfaa8e28503c23a3760ba
Kamil Wójcik <kamil.wojcik@cognifide.com>
2018-05-04 16:47:53 +0200

    Genetic Algorithm for Knapsack - two cases

--- GeneticAlgorithm.js
+++
@@ -1,8 +0,0 @@
-function Item(name,weight,cost,bound) {
-	this.name = name;
-	this.weight = weight;
-	this.cost = cost;
-	this.bound = bound;
-}
-
-// TO DO GA Algorithm
--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ var addElementToDOM = () => {
 }
 
 var initDOMElement = () => {
-  config.popSize = 800;
+  config.popSize = 600; //change to 800
   config.maxGenerations = 200;
   config.maxRuns = 1;
   config.mutateProb = 0.05;
@@ -92,7 +92,7 @@ var runGA = (configGA) => {
     object.fitness = 0;
     if (insertIntoPopulation(object, population, configGA.max_weight)) i++;
   }
-  
+  console.log(population);
 	stop_running = false;
 	geneticAlgorithm(configGA, population);
 }
@@ -106,8 +106,62 @@ var geneticAlgorithm = (configGA, population) => {
 	for (let i = 0; i < population.length; i++) {
     population[i].fitness = measureFitness(population[i].chromosome);
   }
+
+  var newPopulation = new Array();
+	for (var i = 0; i < population.length;) {
+    var randNumber = _.ceil(_.random(1,3));
+    // console.log(randNumber);
+    switch(randNumber) {
+      case 1:
+        //select one individual based on fitness
+        var individual = population[selectFromPopulation(configGA, population)];
+				//perform reproduction
+				var newIndividual = new Object();
+				newIndividual.chromosome = individual.chromosome.slice();
+				newIndividual.fitness = individual.fitness;
+				//insert copy in new pop
+				if(insertIntoPopulation(newIndividual, newPopulation, configGA.items, configGA.max_weight)) i++;
+        break;
+      case 2:
+        //select two individuals based on fitness
+        i++;
+        console.log("case 2");
+        break;
+      case 3:
+        //select one individual based on fitness
+        var individual = population[selectFromPopulation(configGA, population)];
+				//perform mutation
+				var mutant = new Object();
+				mutant.chromosome = individual.chromosome.slice();
+				var r = _.random();
+				var x1 = _.floor(Math.random()*mutant.chromosome.length);
+        var x2 = _.floor(Math.random()*mutant.chromosome.length);
+        
+				if (r < 0.5) {
+					//Mutate 1 - reciprocal exchange
+					var temp = mutant.chromosome[x1];
+					mutant.chromosome[x1] = mutant.chromosome[x2];
+					mutant.chromosome[x2] = temp;
+				} else {
+					//Mutate 2 - insertion
+					var tempC = mutant.chromosome.splice(x1,1);
+					var tempA = mutant.chromosome.splice(x2);
+					mutant.chromosome = mutant.chromosome.concat(tempC.concat(tempA));
+        }
+        
+				mutant.fitness = measureFitness(mutant.chromosome);
+				//insert mutant in new pop
+				if (insertIntoPopulation(mutant, newPopulation, configGA.items, configGA.max_weight)) i++;
+        break;
+      default:
+    }
+  }
   
-  console.log(population);
+  pupulation = newPopulation;
+
+  console.log(pupulation);
+  gen++;
+  // console.log(population);
 }
 
 var insertIntoPopulation = (individual, newPopulation, items, maxWeight) => {
@@ -132,6 +186,33 @@ var insertIntoPopulation = (individual, newPopulation, items, maxWeight) => {
 	return true;
 }
 
+var selectFromPopulation = (config, pop) => {
+  var choices = new Array();
+
+  for (let i = 0 ; i < 5 ; i++) {
+    var rnum = _.floor(Math.random() * pop.length);
+    choices[i] = pop[rnum];
+    choices[i].index = rnum;
+  }
+
+  if (config.fitness_order == "desc") {
+    choices.sort( function (a,b) { return b.fitness-a.fitness });
+  } else {
+    choices.sort( function (a,b) { return a.fitness-b.fitness });
+  }
+
+  var r = _.random(0, 1);
+  //p = 0.5
+  if (r < 0.5) {
+    //return most fit
+    return choices[choices.length-1].index;
+  }
+
+  //otherwise, return a random choice
+  var rnum = _.floor(Math.random() * choices.length);
+  return choices[rnum].index;
+}
+
 var measureFitness = (chromosome) => {
 	var fitness = 0;
 	for (var i = 0; i < chromosome.length; i++) {
@@ -151,7 +232,7 @@ var generateChromosome = (items, maxWeight) => {
   var availableItems = items.slice();
 
 	while (weightSoFar <= maxWeight && availableItems.length) {
-    var index = Math.floor(Math.random() * availableItems.length);
+    var index = _.floor(Math.random() * availableItems.length);
 
 		if ((weightSoFar + availableItems[index].weight) <= maxWeight) {
 			randomChromosome = randomChromosome.concat(availableItems[index]);
